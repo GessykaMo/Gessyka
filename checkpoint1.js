568b579c2cb9d8fe6da2b2a5a01156e243988c2a
Gessykamo <gessykamoraesdasilva@gmail.com>
2021-08-25 22:31:11 -0300

    inclusão

--- checkpoint1.js
+++ checkpoint1.js
@@ -5,7 +5,7 @@ function pipoca(tempo){
     if(tempo<10){
         info_menu ="Tempo Insuficiente";
     }else if(tempo>=10 && tempo <20){
-        info_menu="Prato Perfeito";
+        info_menu="Prato pronto, bom apetite!!!";
     }else if ( tempo >=20 && tempo<30){
         info_menu="Comida Queimou" ;
     }else{
@@ -21,7 +21,7 @@ function macarrao(tempo){
     if(tempo<8){
         info_menu ="Tempo Insuficiente";
     }else if(tempo>=8 && tempo <16){
-        info_menu="Prato Perfeito";
+        info_menu="Prato pronto, bom apetite!!!";
     }else if ( tempo >=16 && tempo<24){
         info_menu="Comida Queimou" ;
     }else{
@@ -37,6 +37,42 @@ function carne(tempo){
     if(tempo<15){
         info_menu ="Tempo Insuficiente";
     }else if(tempo>=15 && tempo <30){
-        info_menu="Prato Perfeito"
+        info_menu="Prato pronto, bom apetite!!!"
+    }else if ( tempo >=30 && tempo<45){
+        info_menu="Comida Queimou" ;
+    }else{
+        info_menu="Kabuumm";
     }
-}    
+    return info_menu;
+}
+
+function feijão(tempo){
+
+    let  info_menu = " ";
+
+    if(tempo<12){
+        info_menu ="Tempo Insuficiente";
+    }else if(tempo>=12 && tempo <24){
+        info_menu="Prato pronto, bom apetite!!!"
+    }else if ( tempo >=24 && tempo<36){
+        info_menu="Comida Queimou" ;
+    }else{
+        info_menu="Kabuumm";
+    }
+    return info_menu;
+}
+function brigadeiro(tempo){
+
+    let  info_menu = " ";
+
+    if(tempo<8){
+        info_menu ="Tempo Insuficiente";
+    }else if(tempo>=8 && tempo <16){
+        info_menu="Prato pronto, bom apetite!!!"
+    }else if ( tempo >=16 && tempo<24){
+        info_menu="Comida Queimou" ;
+    }else{
+        info_menu="Kabuumm";
+    }
+    return info_menu;
+}
