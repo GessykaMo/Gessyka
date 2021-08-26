6373e90b370a7753ce60a2cc1b9152b5f8496e9a
Gessykamo <gessykamoraesdasilva@gmail.com>
2021-08-25 22:17:48 -0300

    inclusão

--- checkpoint1.js
+++ checkpoint1.js
@@ -1,19 +1,42 @@
-function microondas (comida, tempo){
-    let mensagem = '';
-    switch (comida){
-        case 'Pipoca':
-            if(tempo >=10 && tempo <20) {
-                mensagem = 'Prato pronto, bom apetite!!!';
-            }
-            else if (tempo >=20 && tempo <30){
-                mensagem = 'Sua comida queimou';
-            }
-            else if (tempo >= 30){
-                mensagem = 'Kabummm';
-            }
-            else {
-                mensagem
-            }
-    }
+function pipoca(tempo){
 
+    let  info_menu = " ";
+
+    if(tempo<10){
+        info_menu ="Tempo Insuficiente";
+    }else if(tempo>=10 && tempo <20){
+        info_menu="Prato Perfeito";
+    }else if ( tempo >=20 && tempo<30){
+        info_menu="Comida Queimou" ;
+    }else{
+        info_menu="Kabuumm";
+    }
+    return info_menu;
 }
+
+function macarrao(tempo){
+
+    let  info_menu = " ";
+
+    if(tempo<8){
+        info_menu ="Tempo Insuficiente";
+    }else if(tempo>=8 && tempo <16){
+        info_menu="Prato Perfeito";
+    }else if ( tempo >=16 && tempo<24){
+        info_menu="Comida Queimou" ;
+    }else{
+        info_menu="Kabuumm";
+    }
+    return info_menu;
+}
+
+function carne(tempo){
+
+    let  info_menu = " ";
+
+    if(tempo<15){
+        info_menu ="Tempo Insuficiente";
+    }else if(tempo>=15 && tempo <30){
+        info_menu="Prato Perfeito"
+    }
+}    
